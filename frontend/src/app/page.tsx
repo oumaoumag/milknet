5c96f53e413fe177a72873d586ed2dbf31da99d5
oumaoumag <ouma.godwin10@gmail.com>
2025-01-24 00:03:49 +0300

    fix - remove redundant import

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Waitlist from "./pages/waitlist";
 
 export default function Home() {
--- frontend/src/app/pages/waitlist.tsx
+++ frontend/src/app/pages/waitlist.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useState } from 'react';
-import Image from 'next/image';
 import { CheckIcon, ChevronRightIcon, InfoIcon } from 'lucide-react';
 import { motion } from 'framer-motion';
 
