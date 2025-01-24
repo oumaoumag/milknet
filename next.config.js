6558144935c23be73fb174a9c59959f97798cfa8
oumaoumag <ouma.godwin10@gmail.com>
2025-01-24 05:08:52 +0300

    update - dependancies

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// import type { NextConfig } from "next";
+
+const nextConfig = {
+  images: {
+    domains: ['via.placeholder.com'],
+  },
+  experimental: {
+    turbo: true
+  }
+};
+
+export default nextConfig;
